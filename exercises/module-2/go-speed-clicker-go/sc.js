d6ffed4d3b9d27772aca35b1f56324a950eb1df6
Benjamin Vredevelt <bjv753@gmail.com>
2020-02-29 16:03:58 -0700

    go-speed-clicker-update

--- exercises/module-2/go-speed-clicker-go/sc.js
+++ exercises/module-2/go-speed-clicker-go/sc.js
@@ -1,1 +1,21 @@
-// https://www.youtube.com/watch?v=NxVCq4p0Kb0
+const startBtn = document.getElementById("start")
+const stopBtn = document.getElementById("stop")
+const timer = document.getElementById("timer")
+
+let count = 0
+let intervalId
+timer.textContent = count
+
+function incrementTimer(){
+    count++
+    timer.textContent = count
+}
+
+startBtn.addEventListener("click", function(){
+    intervalId = setInterval(incrementTimer, 1000)
+})
+
+
+stopBtn.addEventListener("click", function(){
+    clearInterval(intervalId)
+})
